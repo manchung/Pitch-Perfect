5f5eb0a800a189a84d3f0000aa73791579a24ec8
Manch Hon <manch.hon@gmail.com>
2015-07-22 23:45:55 -0700

    Added a new screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet weak var recordingLabel: UILabel!
     @IBAction func recordAudio(sender: UIButton) {
         // toggle
         recordingLabel.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
     }
 
     @IBAction func stopRecord(sender: AnyObject) {
